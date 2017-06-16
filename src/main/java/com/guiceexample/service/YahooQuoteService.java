c94898ea2f51ae66324a895ad049b533d37b1ef2
Neil A. Ernst <neil@neilernst.net>
2017-06-16 13:53:15 -0400

    updates Yahoo URL

--- src/main/java/com/guiceexample/service/YahooQuoteService.java
+++ src/main/java/com/guiceexample/service/YahooQuoteService.java
@@ -9,9 +9,7 @@ import java.net.URL;
 import java.util.List;
 
 import javax.inject.Inject;
-
 import org.apache.commons.io.IOUtils;
-
 import com.guiceexample.util.AuditLogger;
 
 public class YahooQuoteService implements QuoteService
@@ -23,20 +21,20 @@ public class YahooQuoteService implements QuoteService
 	{
 		this.auditLogger = auditLogger;
 	}
-	
+
 	@Override
 	public double getMidPrice(String currencyPair)
 	{
 		try
 		{
-			URL url = new URL("http://finance.yahoo.com/d/quotes.csv?s=" + currencyPair + "=X&f=sl1d1t1ba&e=.csv");
-			
+			URL url = new URL("http://download.finance.yahoo.com/d/quotes.csv?s="+currencyPair + "=X&f=sl1d1t1ba&e=.csv");
+
 			auditLogger.log("Retrieving a quote from: " + url);
-			
+
 			InputStream stream = url.openStream();
 			List<String> lines = IOUtils.readLines(stream);
 			String[] tokens = lines.get(0).split(",");
-			
+
 			return Double.parseDouble(tokens[1]);
 		}
 		catch (IOException e)
